82b10f838ecf6661a3823c7cb306f29260d83ec3
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2021-03-24 18:59:25 -0400

    Projeto com as pastas de Model, ViewModel e View(sendo essa está com a ContentView.swift)

---
+++ Challenge about film/View/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Challenge about film
+//
+//  Created by Rebeca Pacheco on 24/03/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+
+    var body: some View {
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
