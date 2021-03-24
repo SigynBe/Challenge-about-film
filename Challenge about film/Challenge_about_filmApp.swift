685315f04075086034509e5e2df88ccfbf55723b
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2021-03-24 16:16:27 -0400

    Initial Commit

---
+++ Challenge about film/Challenge_about_filmApp.swift
@@ -0,0 +1,20 @@
+//
+//  Challenge_about_filmApp.swift
+//  Challenge about film
+//
+//  Created by Rebeca Pacheco on 24/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_about_filmApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
